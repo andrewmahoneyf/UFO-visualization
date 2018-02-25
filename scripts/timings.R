22b6885f2ba438efde49733f760788e730577683
Sanghawk <shlee95@uw.edu>
2018-02-25 15:09:26 -0800

    Fixed directory issues

--- scripts/timings.R
+++ scripts/timings.R
@@ -1,16 +1,22 @@
-
-
+## Gets the working directory path for a7-collaboration
+## Using relative path (~/...)
 
 if(Sys.info()[["user"]] == "jacquesdebar") {
-  PROJECTDIR <- file.path("Users", "jacquesdebar", "Documents", "INFO201", "Assignments", "a7-collaboration-PAJJ")
+  PROJECTDIR <- file.path("~", "Documents", "INFO201", "Assignments", "a7-collaboration-PAJJ")
   fileName <- "jacques_results.Rdat"
 } else if(Sys.info()[["user"]] == "pulithsilva") {
-  PROJECTDIR <- file.path("Users", "pulithsilva", "info201b", "a7-collaboration-PAJJ")
+  PROJECTDIR <- file.path("~", "info201b", "a7-collaboration-PAJJ")
   fileName <- "pulith_results.Rdat"
 } else if(Sys.info()[["user"]] == "andrewmahoney-fernandes") {
   PROJECTDIR <- file.path("~", "Desktop", "Info-201", "a7-collaboration-PAJJ")
   fileName <- "andrew_results.Rdat"
-} else (Sys.info()[["user"]] == "sanghawk") {
-  PROJECTDIR <- file.path("Users", "sanghawk", "Documents", "Winter '18", "INFO 201", "a7-collaboration","a7-collaboration-PAJJ")
+} else if (Sys.info()[["user"]] == "sanghawk") {
+  PROJECTDIR <- file.path("~", "Documents", "Winter '18", "INFO 201", "a7-collaboration","a7-collaboration-PAJJ")
   fileName <- "sanghawk_results.Rdat"
 } 
+
+## Sets the instance to the user's appropriate working directory
+setwd(PROJECTDIR)
+getwd()
+
+
