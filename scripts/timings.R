7cac361970d7b0babb85bc6b96238dd391a6a5c7
Sanghawk <shlee95@uw.edu>
2018-02-25 14:31:17 -0800

    James is added

--- scripts/timings.R
+++ scripts/timings.R
@@ -4,4 +4,7 @@
 if(Sys.info()[["user"]] == "jacquesdebar") {
   PROJECTDIR <- file.path("Users", "jacquesdebar", "Documents", "INFO201", "Assignments", "a7-collaboration-PAJJ")
   fileName <- "jacques_results.Rdat"
-}
+} else if (Sys.info()[["user"]] == "sanghawk") {
+  PROJECTDIR <- file.path("Users", "sanghawk", "Documents", "Winter '18", "INFO 201", "a7-collaboration","a7-collaboration-andrewmahoneyf")
+  fileName <- "sanghawk_results.Rdat"
+} 
