7a9cde1bf2834de4264d2d2661c2969628dce627
andrewmahoneyf <andrew.mahoney.f@gmail.com>
2018-02-25 14:37:55 -0800

    Add path to project directory

--- scripts/timings.R
+++ scripts/timings.R
@@ -4,4 +4,7 @@
 if(Sys.info()[["user"]] == "jacquesdebar") {
   PROJECTDIR <- file.path("Users", "jacquesdebar", "Documents", "INFO201", "Assignments", "a7-collaboration-PAJJ")
   fileName <- "jacques_results.Rdat"
+} else if(Sys.info()[["user"]] == "andrewmahoney-fernandes") {
+  PROJECTDIR <- file.path("~", "Desktop", "Info-201", "a7-collaboration-PAJJ")
+  fileName <- "andrew_results.Rdat"
 }
