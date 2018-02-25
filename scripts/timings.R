7674cb14adb3f85c83ade55997f5fc9cb3de874c
Pulith Silva <pulithsilva3@gmail.com>
2018-02-25 14:37:59 -0800

    add changes to timings

--- scripts/timings.R
+++ scripts/timings.R
@@ -5,3 +5,8 @@ if(Sys.info()[["user"]] == "jacquesdebar") {
   PROJECTDIR <- file.path("Users", "jacquesdebar", "Documents", "INFO201", "Assignments", "a7-collaboration-PAJJ")
   fileName <- "jacques_results.Rdat"
 }
+
+if(Sys.info()[["user"]] == "pulithsilva") {
+  PROJECTDIR <- file.path("Users", "pulithsilva", "info201b", "a7-collaboration-PAJJ")
+  fileName <- "pulith_results.Rdat"
+}
