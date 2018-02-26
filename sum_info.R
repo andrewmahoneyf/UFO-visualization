cd72e38f70df55edfd91c09f7b40e658cd05b4dd
Sanghawk <shlee95@uw.edu>
2018-02-25 18:08:50 -0800

    Finished the summary info Rscript file. Returns valuable information for a given dataset

--- sum_info.R
+++ sum_info.R
@@ -1,8 +1,10 @@
 ## A function that takes in a dataset and returns a list of info about it:
-describeData <- function(dataset) {
+DescribeData <- function(dataset) {
   ret <- list()
+  ret$colnames <- colnames(dataset)
   ret$length <- nrow(dataset)
-  ## do some more interesting stuff here
-  ## add more components to 'ret', ...
+  ret$dim <- dim(dataset)
+  ret$states_count <- dataset %>% group_by(State) %>% summarize(n = n())
+  ret$shapes_count <- dataset %>% group_by(Shape) %>% summarize(n = n())
   return(ret)
 }
