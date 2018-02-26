0a01a9ae3c1ca1a1e4d340c5509fa95909765594
Sanghawk <shlee95@uw.edu>
2018-02-25 16:07:29 -0800

    Added the visualizations for the UFOs

--- scripts/process_data.R
+++ scripts/process_data.R
@@ -1,4 +1,4 @@
-install.packages(c("dplyr","maps","data.table", "ggmap", "ggplot2","mapdata","maptools"))
+##install.packages(c("dplyr","maps","data.table", "ggmap", "ggplot2","mapdata","maptools"))
 library(data.table)
 library(dplyr)
 library(maps)
@@ -8,17 +8,20 @@ library(mapdata)
 library(maptools)
 
 ## import static files
-system.time(ufo_data <- fread("../data/UFOCoords.csv"))
+ufo_data <- fread("./data/UFOCoords.csv")
 
 # getting the map
-mapgilbert <- get_map(location = c(lon = mean(ufo_data$lng), lat = mean(ufo_data$lat)), zoom = 4,
+mapgilbert <- get_map(location = c(lon = mean(ufo_data$lng), lat = mean(ufo_data$lat)), zoom = 3,
                       maptype = "satellite", scale = 2)
+
 
 # plotting the map with some points on it
 ufo_map <- ggmap(mapgilbert) +
-  geom_point(data = ufo_data, aes(x = lng, y = lat, fill = "red", alpha = 0.8), size = 2, shape = 21) +
-  guides(fill=FALSE, alpha=FALSE, size=FALSE)
+  geom_point(data = ufo_data, aes(x = lng, y = lat, fill = "red", alpha = 0.8), size = 1, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE) +
+  labs(x="Longitude", y="Latitude")
+ggsave("./img/ufo_map.jpg", ufo_map)
 
 # plots
-shapes <- ufo_data$Shape
 ufo_shapes <- ggplot(ufo_data, aes(Shape)) + geom_bar()
+ggsave("./img/ufo_shapes.jpg", ufo_shapes)
